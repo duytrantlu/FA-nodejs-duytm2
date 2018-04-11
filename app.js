e5863e22794886dbb5f2f6ca55cf78b8fb0b9c4c
DuyTran <duytrantlu@gmail.com>
2018-04-11 21:43:17 +0700

    app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+ function uniteUnique(...arr){
+  let newArr = [];
+  let i = 0;
+  const s = new Set();
+  while (arr[i]) {
+    newArr = newArr.concat(arr[i]);
+    i++;
+  }
+  for (let j = 0; j < newArr.length; j++) {
+    s.add(newArr[j]);
+  }
+  return Array.from(s);
+};
+//console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
+
+ function booWho(bool) {
+  return (typeof bool) ==="boolean";
+}
+
+//console.log(booWho(null));
+export{uniteUnique,booWho}
