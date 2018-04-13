dabfe12b87e904a6acd739e72cce883d9003e24e
DuyTran <duytrantlu@gmail.com>
2018-04-13 14:59:39 +0700

    update -assignment 02

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// bài 1:
 function uniteUnique(...arr) {
   let newArr = [];
   let i = 0;
@@ -12,10 +13,35 @@ function uniteUnique(...arr) {
   return Array.from(s);
 }
 // console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
-
+// bài 2
 function booWho(bool) {
   return typeof bool === 'boolean';
 }
 
 // console.log(booWho(null));
-export { uniteUnique, booWho };
+
+// bài 3:
+
+function convertHTML(str) {
+  // &colon;&rpar;
+  const regEx = /[&<>"']/g;
+  const obj = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&apos;',
+  };
+  const placer = match => obj[match];
+  const rsStr = str.replace(regEx, placer);
+  return rsStr;
+}
+
+// console.log(convertHTML("Dolce & Gabbana <>"));
+// bài 4:
+function spinalCase(str) {
+  return str.replace(/([a-z])([A-Z])|[(\s)|_|\W]+/g, '$1-$2').toLowerCase();
+}
+// console.log(spinalCase('This::IsSpinalTap'));
+
+export { uniteUnique, booWho, convertHTML, spinalCase };
